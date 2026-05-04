1b5251a0160bf1ca6c5b68de3a2c5ff4acf882c1
Ivan <ivanmendes2005@gmail.com>
2026-05-04 10:41:24 -0300

    feat: add django

--- tasks.py
+++ tasks.py
@@ -44,6 +44,32 @@ def format():
     )
 
 
+def make_migrations():
+    subprocess.run(["uv", "run", "python", "src/manage.py", "makemigrations"])
+
+
+def migrate():
+    subprocess.run(["uv", "run", "python", "src/manage.py", "migrate"])
+
+
+def runserver():
+    subprocess.run(["uv", "run", "python", "src/manage.py", "runserver"])
+
+
+def createsuperuser():
+    subprocess.run(["uv", "run", "python", "src/manage.py", "createsuperuser"])
+
+
+def collectstatic():
+    subprocess.run(
+        ["uv", "run", "python", "src/manage.py", "collectstatic", "--noinput"]
+    )
+
+
+def shell():
+    subprocess.run(["uv", "run", "python", "src/manage.py", "shell"])
+
+
 if __name__ == "__main__":
     arg = sys.argv[1] if len(sys.argv) > 1 else ""
     if arg == "test":
@@ -54,5 +80,30 @@ if __name__ == "__main__":
         lint()
     elif arg == "format":
         format()
+    elif arg == "mkmig":
+        make_migrations()
+    elif arg == "mig":
+        migrate()
+    elif arg == "run":
+        runserver()
+    elif arg == "csu":
+        createsuperuser()
+    elif arg == "cs":
+        collectstatic()
+    elif arg == "shell":
+        shell()
     else:
-        print("Usage: python tasks.py [test|tests_cov|lint|format]")
+        print(
+            "Usage: python tasks.py "
+            "["
+            "test|"
+            "tests_cov|"
+            "lint|"
+            "format|"
+            "makemigrations|"
+            "migrate|"
+            "runserver|"
+            "createsuperuser|"
+            "collectstatic|"
+            "shell]"
+        )
