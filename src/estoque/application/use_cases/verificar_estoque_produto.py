2b7dd4f2ba9a9e8944fa8f8974237332716801dd
Ivan <ivanmendes2005@gmail.com>
2026-05-06 12:32:50 -0300

    fix: usar id no verificarEstoque

--- src/estoque/application/use_cases/verificar_estoque_produto.py
+++ src/estoque/application/use_cases/verificar_estoque_produto.py
@@ -1,7 +1,6 @@
-from estoque.application.dtos.InfoEstoqueDTO import InfoEstoqueDTO
+from estoque.application.dtos.info_estoque_dto import InfoEstoqueDTO
 from estoque.domain.exceptions import ProdutoIndisponivelError
 from estoque.domain.repositories import IEstoqueRepository
-from produto.domain.entities import Produto
 from produto.domain.repositories import IProdutoRepository
 
 
@@ -13,14 +12,14 @@ class VerificarEstoqueProdutoUseCase:
         self.estoque_repo = estoque_repo
         self.produto_repo = produto_repo
 
-    def execute(self, produto: Produto) -> InfoEstoqueDTO:
+    def execute(self, produto_id: str) -> InfoEstoqueDTO:
 
-        item = self.estoque_repo.obter_item_estoque(produto.id)
+        item = self.estoque_repo.obter_item_estoque(produto_id)
 
         if item is None:
             raise ProdutoIndisponivelError("Produto não encontrado no estoque!")
 
-        produto_banco = self.produto_repo.obter_produto(produto.id)
+        produto_banco = self.produto_repo.obter_produto(produto_id)
         if produto_banco is None:
             raise ProdutoIndisponivelError("Produto não encontrado no repositório!")
 
